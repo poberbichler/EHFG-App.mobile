b72817fd14a8fbef1bc2179ea79b771a55a35fa3
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2016-12-10 21:27:32 +0100

    added navigation via the sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import {Platform, MenuController} from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, Nav} from 'ionic-angular';
+import {StatusBar, Splashscreen} from 'ionic-native';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import {TabsPage} from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.template.html'
@@ -10,12 +10,16 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class EhfgApp {
   rootPage = TabsPage;
 
+  @ViewChild(Nav) nav: Nav;
+
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  openTab(tabIndex: number): void {
+    this.nav.setRoot(TabsPage, tabIndex);
   }
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,6 +4,7 @@ import {TwitterPage} from '../twitter/twitter';
 import {SessionsPage} from '../sessions/sessions.component';
 import {SpeakersPage} from '../speakers/speakers.component';
 import {MapPage} from "../map/map";
+import {NavParams} from "ionic-angular";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,4 +14,10 @@ export class TabsPage {
   tab2Root: any = SessionsPage;
   tab3Root: any = SpeakersPage;
   tab4Root: any = MapPage;
+
+  selectedIndex: number;
+
+  constructor(navParams: NavParams) {
+    this.selectedIndex = navParams.data || 0;
+  }
 }
