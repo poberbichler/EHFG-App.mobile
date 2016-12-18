88500ed15ea347bf5de5e5ad172683e434de2be4
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2016-12-18 09:41:42 +0100

    added about dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
-import {Platform, Nav} from 'ionic-angular';
+import {Platform, Nav, AlertController} from 'ionic-angular';
 import {StatusBar, Splashscreen} from 'ionic-native';
 
 import {TabsPage} from '../pages/tabs/tabs';
@@ -12,7 +12,7 @@ export class EhfgApp {
 
   @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private alertCtrl: AlertController) {
     platform.ready().then(() => {
       StatusBar.styleDefault();
       Splashscreen.hide();
@@ -21,5 +21,20 @@ export class EhfgApp {
 
   openTab(tabIndex: number): void {
     this.nav.setRoot(TabsPage, tabIndex);
+  }
+
+  openAbout(): void {
+    let alert = this.alertCtrl.create({
+      title: 'About',
+      subTitle: `<p>The European Health Forum Gastein is an annual international conference where stakeholders within
+		            the field of healthcare and public health meet to discuss a broad spectrum of important topics.</p>
+		            <p>The theme of this year is "Demographics and Diversity in Europe - New Solutions for Health"</p>
+		            <p>Please direct any questions or concerns to <a href="mailto:info@ehfg.org">info@ehfg.org</a></p>`,
+      buttons: ['Exit']});
+    alert.present();
+  }
+
+  resetData(): void {
+    console.log('reset is not implemented yet');
   }
 }
