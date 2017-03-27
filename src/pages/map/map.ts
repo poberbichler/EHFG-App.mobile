7ff5badf6afd47d08bcef5747496bf67ef288fb6
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2017-03-27 20:30:03 +0200

    added custom google map markers and infoWindows

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,9 @@
 import {Component, ViewChild, ElementRef} from "@angular/core";
 import {Platform} from "ionic-angular";
+import {GoogleMap} from "@ionic-native/google-maps";
+import {Http} from "@angular/http";
+
+import 'rxjs/add/operator/toPromise';
 
 declare var google: any;
 
@@ -10,17 +14,54 @@ declare var google: any;
 export class MapPage {
   @ViewChild('mapCanvas') mapElement: ElementRef;
 
-  constructor(private platform: Platform) { }
+  public map: GoogleMap;
+
+  constructor(private platform: Platform, private http: Http) { }
 
   ionViewDidLoad(): void {
+    console.log(this.platform);
     if (this.platform.is('cordova') === true) {
-      console.log("native map not implemented yet :(");
+      console.log('hm...')
+      /*
+      let mapEle = this.mapElement.nativeElement;
+      this.map = new GoogleMap('map_canvas');
+      mapEle.classList.add('show-map');
+
+      GoogleMap.isAvailable().then(() => {
+        console.log('hmm2...')
+        this.map.moveCamera({
+          target: new GoogleMaps(47.170329, 13.103852),
+          zoom: 16
+        });
+      });
+      */
     } else {
+      console.log('not cordova, using browser based googleMap');
       let mapEle = this.mapElement.nativeElement;
 
       let map = new google.maps.Map(mapEle, {
         center: {lat: 47.170329, lng: 13.103852},
         zoom: 16
+      });
+
+      this.http.get(`https://backend-ehfg.rhcloud.com/rest/points`).subscribe(data => {
+        data.json().forEach(point => {
+          console.log(point);
+
+          let marker = new google.maps.Marker({
+            position: {lat: point.coordinate.latitude, lng: point.coordinate.longitude},
+            map: map,
+            icon: `assets/img/markers/${point.category.cssClass ? point.category.cssClass + '-' : ''}marker.png`
+          });
+
+          marker.addListener('click', () => {
+            let infoWindow = new google.maps.InfoWindow({
+              content: `<p>${point.description}</p>`
+            });
+
+            infoWindow.open(map, marker);
+          });
+        });
       });
     }
   }
